1007b4a97764fcbfbf9b482debd50edef9f75e99
Your Name <you@example.com>
2023-01-13 07:38:40 +0600

    4 lab

---
+++ labka4/Car.py
@@ -0,0 +1,16 @@
+
+class Car:
+
+    def stop(self):
+        print("Останавливаемся")
+
+    def start(self):
+        print("Поехали")
+
+    def turnRight(self):
+        print("Поворот направо")
+
+    def turnLeft(self):
+        print("Поворот налево")
+
+
